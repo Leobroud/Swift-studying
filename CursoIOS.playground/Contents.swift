95a797b64efbcbb4a69eb01bfd864bbf271bd122
leonardo.barbosa <leo.oliveira@WP05213NRJBA.local>
2022-01-21 21:31:20 -0300

    aula de repetição, for, repeat e while

--- CursoIOS.playground/Contents.swift
+++ CursoIOS.playground/Contents.swift
@@ -1,8 +1,50 @@
 import UIKit
 
-var greeting = "Hello, Leo"
+/*var greeting = "Hello, Leo"
 
 2 + 5
 
 var nomes = ["1", "2", "3"]
 print(nomes[0])
+*/
+
+//Loops - for
+
+/*for i in 0..<6 {
+    print("Número: " + String(i))
+}
+
+
+var comentarios: [String] = []
+comentarios.append("Teste um")
+comentarios.append("Teste dois")
+comentarios.append("Teste Três")
+
+for comentario in comentarios {
+    print(comentario)
+}
+*/
+
+// While
+
+
+/*
+ 
+ var contador: Int = 0
+
+while (contador < 6) {
+    
+    print(contador)
+    
+    contador += 1
+}
+ 
+
+repeat {
+    
+    print(contador)
+    contador += 1
+    
+} while contador < 6
+
+ */
