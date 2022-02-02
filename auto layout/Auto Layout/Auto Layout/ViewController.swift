6a424045cb93bbb85081a59ac36e1442772f94e0
leonardo.barbosa <leonardo.barbosa@g.globo>
2022-02-02 17:57:08 -0300

    desenvolvimento ios

---
+++ auto layout/Auto Layout/Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by leonardo.barbosa on 01/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
